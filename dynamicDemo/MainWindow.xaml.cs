4b6e363f28ce9c0297f20a0a3e5f5d543ebd6421
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-08-24 23:28:39 +0900

    Add readme

--- dynamicDemo/MainWindow.xaml.cs
+++ dynamicDemo/MainWindow.xaml.cs
@@ -9,7 +9,11 @@ namespace DynamicDemo {
     public partial class MainWindow : Window {
 
         private DispatcherTimer dispatcherTimer;
-        private int countUp = 0;
+
+        private int windowWidth = 525;
+
+        private double catleft = 0;
+        private double catright = 200;
 
         public MainWindow() {
             InitializeComponent();
@@ -20,10 +24,11 @@ namespace DynamicDemo {
         /// 一定時間でDispatcherTimerTickを実行する関数
         /// </summary>
         private void FixedTimeProcess() {
-            //初期化
+            // 初期化
             dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal);
-            //左から　日数、時間、分、秒、ミリ秒で設定
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            // 左から　日数、時間、分、秒、ミリ秒で設定
+            // 1ミリ秒ごとに30回実行
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
             dispatcherTimer.Tick += new EventHandler(DispatcherTimerTick);
             dispatcherTimer.Start();
         }
@@ -32,12 +37,19 @@ namespace DynamicDemo {
         /// FixedTimeProcessで実行される関数
         /// </summary>
         private void DispatcherTimerTick(object sender, EventArgs e) {
-            countUp++;
-            this.Resources["count"] = countUp;
+
+            catleft = (catleft < windowWidth / 2) ? catleft + 1 : -40;
+            catright = (catleft < windowWidth / 2) ? catright - 1 : 240;
+
+            MovePic(catleft, 84, catright, 84, "catMargin");
         }
 
+        /// <summary>
+        /// テキストの大きさを変える関数
+        /// </summary>
+        private void MovePic(double left, double top, double right, double bottom, string target) {
+            Thickness margin = new Thickness(left, top, right, bottom);
+            this.Resources[target] = margin;
+        }
     }
-
-
-
 }
